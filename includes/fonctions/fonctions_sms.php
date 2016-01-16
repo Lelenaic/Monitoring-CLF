f8083a29e0aceb971dbc38068028e120a7ff970f
lelenaic <lenaic@lenaic.me>
2016-01-16 15:36:39 +0100

    Update fonctions_sms.php

--- includes/fonctions/fonctions_sms.php
+++ includes/fonctions/fonctions_sms.php
@@ -4,9 +4,9 @@ function sms($id,$erreur=''){
     $infoServices=Connexion::query('SELECT services.id,services.nom,services.tel,tests.date,tests.heure,tests.trl,tests.codeHttp FROM tests,services WHERE tests.service_id=services.id AND tests.id='.$id.''); 
     $tel=$infoServices[0][2];
     if($erreur!=''){
-        $sms='http://www.sms-lowcost.com/cgi-bin/?keyid=c163164881ddcaa39ab7767725742a99&num='.$tel.'&sms='.urlencode('[Monitoring Tridemark] Le service '.$infoServices[0][1].' est maintenant rétabli (id:'.$infoServices[0][0].')');
+        $sms='http://www.sms-lowcost.com/cgi-bin/?keyid=&num='.$tel.'&sms='.urlencode('[Monitoring Tridemark] Le service '.$infoServices[0][1].' est maintenant rétabli (id:'.$infoServices[0][0].')');
     }else{
-        $sms='http://www.sms-lowcost.com/cgi-bin/?keyid=c163164881ddcaa39ab7767725742a99&num='.$tel.'&sms='.urlencode('[Monitoring Tridemark] Erreur sur le service '.$infoServices[0][1].' (id:'.$infoServices[0][0].')');
+        $sms='http://www.sms-lowcost.com/cgi-bin/?keyid=&num='.$tel.'&sms='.urlencode('[Monitoring Tridemark] Erreur sur le service '.$infoServices[0][1].' (id:'.$infoServices[0][0].')');
     }
     $fichier=file($sms);
     if (substr($fichier[0],55,1)==0){
